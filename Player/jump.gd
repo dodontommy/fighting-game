98140b3224ee06933d57a3dc5c16846925d593f3
Tommy <machine.valley@gmail.com>
2024-11-17 21:48:44 -0600

    clean up my jump

--- Player/jump.gd
+++ Player/jump.gd
@@ -1,15 +1,14 @@
 class_name PlayerJumpState
 extends PlayerState
 
-const BASE_FORCE: float = 2.5
-const JUMP_FRAMES: int = 30
+const BASE_FORCE: float = -250
 
 var jump_frame_count: int = 0
 
 func enter() -> void:
 	super()
 	jump_frame_count = 0
-	player.velocity.y -= BASE_FORCE
+	player.velocity.y = BASE_FORCE
 	player.animation.play(jump_anim)
 
 func exit(new_state: State = null) -> void:
@@ -18,10 +17,7 @@ func exit(new_state: State = null) -> void:
 
 func process_physics(delta: float) -> State:
 	super(delta)
-	jump_frame_count += 1
-	if jump_frame_count >= JUMP_FRAMES:
-		return fall_state
-	player.velocity.y -= (gravity * delta) / BASE_FORCE
-	print('jump frame count: ' + str(jump_frame_count))
+	if player.velocity.y >= 0: return fall_state
+	player.velocity.y += (gravity * delta)
 	player.move_and_slide()
 	return null
