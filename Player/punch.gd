ede0e01f8e9121111877180057026950b2c94b3f
Tommy <machine.valley@gmail.com>
2024-11-24 01:15:39 -0600

    fix some animation stuff wit hpunch

--- Player/punch.gd
+++ Player/punch.gd
@@ -2,10 +2,11 @@ class_name PlayerPunchState
 extends PlayerState
 
 var has_attacked: bool = false
+var frame_count: int = 0
 
 func enter() -> void:
 	super()
-	print(Engine.get_frames_per_second()  )
+	frame_count = 0
 	has_attacked = false
 	player.velocity.x = 0
 	player.animation.play(punch_anim)
@@ -13,8 +14,10 @@ func enter() -> void:
 
 func exit(new_state: State = null) -> void:
 	super(new_state)
+	print("Punch state was active for %d frames" % frame_count)  # Output the frame count
 
 func process_frame(_delta: float) -> State:
+	frame_count += 1
 	if has_attacked:
 		if Input.is_action_pressed(player.left_key) or Input.is_action_pressed(player.right_key):
 			return walk_state
